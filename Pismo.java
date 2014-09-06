f6859b3fbd1b6e95492a3de46f814a92435ba7b0
Jovo Krneta <Jovan1982@users.noreply.github.com>
2014-09-06 23:41:30 +0200

    Create Pismo.java

---
+++ Pismo.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lang;
+
+/**
+ *
+ * @author Jovo
+ */
+public enum Pismo {
+  Lat,Cyr  
+}
