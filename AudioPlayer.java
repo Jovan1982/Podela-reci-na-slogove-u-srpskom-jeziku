d6d5335279399e7a6f9a28b4e966092c194fd4a3
Jovo Krneta <Jovan1982@users.noreply.github.com>
2014-09-18 11:39:17 +0200

    Create AudioPlayer.java
    Klasa za snimanje i reprodukovanje audio zapisa

---
+++ AudioPlayer.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lang;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.sound.sampled.*;
+
+/**
+ *
+ * @author jkrne
+ */
+public class AudioPlayer {
+
+    ByteArrayOutputStream out;
+    protected boolean running;
+
+    public void captureAudio() {
+        try {
+            final AudioFormat format = getFormat();
+            DataLine.Info info = new DataLine.Info(
+                    TargetDataLine.class, format);
+            final TargetDataLine line = (TargetDataLine) AudioSystem.getLine(info);
+            line.open(format);
+            line.start();
+            Runnable runner = new Runnable() {
+                int bufferSize = (int) format.getSampleRate()
+                        * format.getFrameSize();
+                byte buffer[] = new byte[bufferSize];
+
+                public void run() {
+                    out = new ByteArrayOutputStream();
+                    running = true;
+                    try {
+                        while (running) {
+                            int count
+                                    = line.read(buffer, 0, buffer.length);
+                            if (count > 0) {
+                                out.write(buffer, 0, count);
+                            }
+                        }
+                        out.close();
+                    } catch (IOException e) {
+                        System.err.println("I/O problems: " + e);
+                        System.exit(-1);
+                    }
+                }
+            };
+            Thread captureThread = new Thread(runner);
+            captureThread.start();
+        } catch (LineUnavailableException e) {
+            System.err.println("Line unavailable: " + e);
+            System.exit(-2);
+        }
+    }
+
+    public void stop() {
+        running = false;
+    }
+
+    public void playAudio() {
+        try {
+            byte audio[] = out.toByteArray();
+            InputStream input
+                    = new ByteArrayInputStream(audio);
+            final AudioFormat format = getFormat();
+            final AudioInputStream ais
+                    = new AudioInputStream(input, format,
+                            audio.length / format.getFrameSize());
+            DataLine.Info info = new DataLine.Info(
+                    SourceDataLine.class, format);
+            final SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
+            line.open(format);
+            line.start();
+
+            Runnable runner = new Runnable() {
+                int bufferSize = (int) format.getSampleRate()
+                        * format.getFrameSize();
+                byte buffer[] = new byte[bufferSize];
+
+                public void run() {
+                    try {
+                        int count;
+                        while ((count = ais.read(
+                                buffer, 0, buffer.length)) != -1) {
+                            if (count > 0) {
+                                line.write(buffer, 0, count);
+                            }
+                        }
+                        line.drain();
+                        line.close();
+                    } catch (IOException e) {
+                        System.err.println("I/O problems: " + e);
+                        System.exit(-3);
+                    }
+                }
+            };
+            Thread playThread = new Thread(runner);
+            playThread.start();
+        } catch (LineUnavailableException e) {
+            System.err.println("Line unavailable: " + e);
+            System.exit(-4);
+        }
+    }
+
+    private AudioFormat getFormat() {
+        float sampleRate = 8000;
+        int sampleSizeInBits = 8;
+        int channels = 1;
+        boolean signed = true;
+        boolean bigEndian = true;
+        return new AudioFormat(sampleRate,
+                sampleSizeInBits, channels, signed, bigEndian);
+    }
+
+}
